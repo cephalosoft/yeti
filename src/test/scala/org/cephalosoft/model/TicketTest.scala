004946c2c82585131cbc6c8c1877f92d14605c34
Benjamin Gramlich <benjamin@redwoolf.net>
2011-01-23 20:07:48 -0600

    added tests for ticket test

--- src/test/scala/org/cephalosoft/model/TicketTest.scala
+++ src/test/scala/org/cephalosoft/model/TicketTest.scala
@@ -8,7 +8,20 @@ class TicketTest {
 
   @Test def addItemToTicket() {
     var ticket: Ticket = new Ticket()
+    ticket.item("Widget")
     assertEquals(ticket.item, "Widget")
+  }
+  
+  @Test def addRequestorToTicket() {
+    var ticket: Ticket = new Ticket()
+    ticket.requestor("Benjamin")
+    assertEquals(ticket.requestor, "Benjamin")
+  }
+
+  @Test def addHandlerToTicket() {
+    var ticket: Ticket = new Ticket()
+    ticket.handler("Jeremiah")
+    assertEquals(ticket.handler, "Jeremiah")
   }
 }
 }
