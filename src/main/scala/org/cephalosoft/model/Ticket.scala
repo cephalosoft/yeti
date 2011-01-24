a72360182ee8cc07664e91dafbcf991b98a8fbc3
Benjamin Gramlich <benjamin@redwoolf.net>
2011-01-23 20:54:24 -0600

    CreateItem class

--- src/main/scala/org/cephalosoft/model/Ticket.scala
+++ src/main/scala/org/cephalosoft/model/Ticket.scala
@@ -4,16 +4,15 @@ import net.liftweb.mapper._
 import net.liftweb.util._
 import net.liftweb.common._
 
-object TicketMetaData extends Ticket with KeyedMetaMapper[Long, Ticket] with CRUDify[Long, Ticket] {
+object Ticket extends Ticket with KeyedMetaMapper[Long, Ticket] with CRUDify[Long, Ticket] {
   override def dbTableName = "tickets"
-  override def fieldOrder = List(requestor, handler, item)
 }
 
-class Ticket extends KeyedMapper[Long, Ticket] {
+class Ticket extends KeyedMapper[Long, Ticket] with OneToMany [Long, Ticket]{
   def getSingleton = TicketMetaData
   def primaryKeyField = id
   object id extends MappedLongIndex(this)
   object requestor extends MappedText(this)
   object handler extends MappedText(this)
-  object item extends MappedText(this)
+  object items extends MappedOneToMany(Item, Item.ticket, OrderBy(Item.id, Ascending))
 }
--- src/test/scala/org/cephalosoft/model/TicketTest.scala
+++ src/test/scala/org/cephalosoft/model/TicketTest.scala
@@ -2,14 +2,17 @@ package org.cephalosoft.model {
 
 import org.junit._
 import Assert._
-import org.cephalosoft.model.Ticket
 
 class TicketTest {
 
   @Test def addItemToTicket() {
-    var ticket: Ticket = new Ticket()
-    ticket.item("Widget")
-    assertEquals(ticket.item, "Widget")
+    val ticket: Ticket = new Ticket()
+    val item: Item = Item.create.ticket(ticket).number(12345).description("Widget")
+    item.save
+    ticket.items += item
+    ticket.save
+    assertEquals(ticket.items.head.description, "Widget")
+    assertEquals(ticket.items.head.number, 12345)
   }
   
   @Test def addRequestorToTicket() {
